5e439e8f0f8b7329f5de06ab7cee5f3ebc98468e
ike-dai <dai.ikd123@gmail.com>
2016-02-05 20:38:16 +0900

    fix get EC2 list bug

--- zaws.go
+++ zaws.go
@@ -16,6 +16,49 @@ import (
 	"time"
 )
 
+type Zaws struct {
+	Region      string
+	AccessKeyId string
+	SecretKeyId string
+	TargetId    string
+	MetricName  string
+	ZabbixHost  string
+	ZabbixPort  string
+	AwsSession  *session.Session
+}
+
+func NewZaws() *Zaws {
+	zaws := new(Zaws)
+	zaws.SetOption()
+	zaws.AwsSession = session.New(&aws.Config{
+		Region:      aws.String(zaws.Region),
+		Credentials: credentials.NewStaticCredentials(zaws.AccessKeyId, zaws.SecretKeyId, ""),
+	})
+	return zaws
+}
+
+func (z *Zaws) SetOption() {
+	flag.StringVar(&z.Region, "region", "ap-northeast-1", "Set AWS region")
+	flag.StringVar(&z.Region, "r", "ap-northeast-1", "Set AWS region")
+	flag.StringVar(&z.AccessKeyId, "key", os.Getenv("AWS_ACCESS_KEY_ID"), "Set AWS API Access key id")
+	flag.StringVar(&z.AccessKeyId, "k", os.Getenv("AWS_ACCESS_KEY_ID"), "Set AWS API Access key id")
+	flag.StringVar(&z.SecretKeyId, "secret", os.Getenv("AWS_SECRET_ACCESS_KEY"), "Set AWS API Secret key id")
+	flag.StringVar(&z.SecretKeyId, "s", os.Getenv("AWS_SECRET_ACCESS_KEY"), "Set AWS API Secret key id")
+	flag.StringVar(&z.TargetId, "id", "", "Set target object id")
+	flag.StringVar(&z.TargetId, "i", "", "Set target object id")
+	flag.StringVar(&z.MetricName, "metric", "", "Set metric name")
+	flag.StringVar(&z.MetricName, "m", "", "Set metric name")
+	flag.StringVar(&z.ZabbixHost, "host", "localhost", "Set zabbix host name")
+	flag.StringVar(&z.ZabbixHost, "h", "localhost", "Set zabbix host name")
+	flag.StringVar(&z.ZabbixPort, "port", "10051", "Set zabbix host port")
+	flag.StringVar(&z.ZabbixPort, "p", "10051", "Set zabbix host port")
+	flag.Parse()
+	if z.AccessKeyId == "" || z.SecretKeyId == "" {
+		fmt.Println("[ERROR]: Please set key information")
+		usage()
+	}
+}
+
 // Declare Struct
 type LldJson struct {
 	Data []Data `json:"data"`
@@ -32,36 +75,6 @@ type Data struct {
 }
 
 // Common util
-func parse_option() {
-	flag.StringVar(&region, "region", "ap-northeast-1", "Set AWS region")
-	flag.StringVar(&region, "r", "ap-northeast-1", "Set AWS region")
-	flag.StringVar(&access_key_id, "key", os.Getenv("AWS_ACCESS_KEY_ID"), "Set AWS API Access key id")
-	flag.StringVar(&access_key_id, "k", os.Getenv("AWS_ACCESS_KEY_ID"), "Set AWS API Access key id")
-	flag.StringVar(&secret_key_id, "secret", os.Getenv("AWS_SECRET_ACCESS_KEY"), "Set AWS API Secret key id")
-	flag.StringVar(&secret_key_id, "s", os.Getenv("AWS_SECRET_ACCESS_KEY"), "Set AWS API Secret key id")
-	flag.StringVar(&target_id, "id", "", "Set target object id")
-	flag.StringVar(&target_id, "i", "", "Set target object id")
-	flag.StringVar(&metric_name, "metric", "", "Set metric name")
-	flag.StringVar(&metric_name, "m", "", "Set metric name")
-	flag.StringVar(&zabbix_host, "host", "localhost", "Set zabbix host name")
-	flag.StringVar(&zabbix_host, "h", "localhost", "Set zabbix host name")
-	flag.StringVar(&zabbix_port, "port", "10051", "Set zabbix host port")
-	flag.StringVar(&zabbix_port, "p", "10051", "Set zabbix host port")
-	flag.Parse()
-	if access_key_id == "" || secret_key_id == "" {
-		fmt.Println("[ERROR]: Please set key information")
-		usage()
-	}
-
-}
-
-func new_session(region, access_key_id, secret_key_id string) *session.Session {
-	sess := session.New(&aws.Config{
-		Region:      aws.String(region),
-		Credentials: credentials.NewStaticCredentials(access_key_id, secret_key_id, ""),
-	})
-	return sess
-}
 
 func usage() {
 	fmt.Println("Usage: zaws service method [target] [-region|-r] [-key|-k] [-secret|-s] [-id|-i] [-metric|-m] [-host|h] [-port|p]")
@@ -75,7 +88,7 @@ func convert_to_lldjson_string(data []Data) string {
 }
 
 // Access AWS API
-func get_metric_list(sess *session.Session) []*cloudwatch.Metric {
+func get_metric_list(sess *session.Session, target_id string) []*cloudwatch.Metric {
 	var metrics []*cloudwatch.Metric
 	svc := cloudwatch.New(sess)
 	params := &cloudwatch.ListMetricsInput{
@@ -95,7 +108,7 @@ func get_metric_list(sess *session.Session) []*cloudwatch.Metric {
 	return metrics
 }
 
-func get_metric_stats(sess *session.Session, metric_name string, metric_namespace string) []*cloudwatch.Datapoint {
+func get_metric_stats(sess *session.Session, target_id, metric_name, metric_namespace string) []*cloudwatch.Datapoint {
 
 	var datapoints []*cloudwatch.Datapoint
 	svc := cloudwatch.New(sess)
@@ -133,16 +146,15 @@ func get_ec2_list(sess *session.Session) []*ec2.Instance {
 		os.Exit(1)
 	}
 	for _, reservation := range resp.Reservations {
-		instances = reservation.Instances
-		return instances
+		instances = append(instances, reservation.Instances...)
 	}
 	return instances
 }
 
 // zaws method
-func show_ec2_list(sess *session.Session) {
+func (z *Zaws) ShowEc2List() {
 	list := make([]Data, 0)
-	instances := get_ec2_list(sess)
+	instances := get_ec2_list(z.AwsSession)
 	for _, instance := range instances {
 		for _, tag := range instance.Tags {
 			if *tag.Key == "Name" {
@@ -158,11 +170,11 @@ func show_ec2_list(sess *session.Session) {
 	fmt.Printf(convert_to_lldjson_string(list))
 }
 
-func show_cloudwatch_metrics_list(sess *session.Session) {
+func (z *Zaws) ShowCloudwatchMetricsList() {
 	list := make([]Data, 0)
-	metrics := get_metric_list(sess)
+	metrics := get_metric_list(z.AwsSession, z.TargetId)
 	for _, metric := range metrics {
-		datapoints := get_metric_stats(sess, *metric.MetricName, *metric.Namespace)
+		datapoints := get_metric_stats(z.AwsSession, z.TargetId, *metric.MetricName, *metric.Namespace)
 		data := Data{MetricName: *metric.MetricName, MetricNamespace: *metric.Namespace}
 		if len(datapoints) > 0 {
 			data.MetricUnit = *datapoints[0].Unit
@@ -173,19 +185,19 @@ func show_cloudwatch_metrics_list(sess *session.Session) {
 	fmt.Printf(convert_to_lldjson_string(list))
 }
 
-func send_metric_stats(sess *session.Session) {
+func (z *Zaws) SendMetricStats() {
 	var send_data []zabbix_sender.DataItem
 
-	metrics := get_metric_list(sess)
+	metrics := get_metric_list(z.AwsSession, z.TargetId)
 	for _, metric := range metrics {
-		datapoints := get_metric_stats(sess, *metric.MetricName, *metric.Namespace)
+		datapoints := get_metric_stats(z.AwsSession, z.TargetId, *metric.MetricName, *metric.Namespace)
 
 		if len(datapoints) > 0 {
 			data_time := *datapoints[0].Timestamp
-			send_data = append(send_data, zabbix_sender.DataItem{Hostname: target_id, Key: "cloudwatch.metric[" + *metric.MetricName + "]", Value: strconv.FormatFloat(*datapoints[0].Average, 'f', 4, 64), Timestamp: data_time.Unix()})
+			send_data = append(send_data, zabbix_sender.DataItem{Hostname: z.TargetId, Key: "cloudwatch.metric[" + *metric.MetricName + "]", Value: strconv.FormatFloat(*datapoints[0].Average, 'f', 4, 64), Timestamp: data_time.Unix()})
 		}
 	}
-	addr, _ := net.ResolveTCPAddr("tcp", zabbix_host+":"+zabbix_port)
+	addr, _ := net.ResolveTCPAddr("tcp", z.ZabbixHost+":"+z.ZabbixPort)
 	res, err := zabbix_sender.Send(addr, send_data)
 	if err != nil {
 		fmt.Printf("[ERROR]: zabbix sender error!: %s", err)
@@ -193,8 +205,6 @@ func send_metric_stats(sess *session.Session) {
 	}
 	fmt.Printf("[INFO]: Successful sending data to Zabbix: resp", res)
 }
-
-var region, access_key_id, secret_key_id, target_id, metric_name, zabbix_host, zabbix_port string
 
 func main() {
 	if len(os.Args) < 3 {
@@ -205,8 +215,8 @@ func main() {
 		switch os.Args[2] {
 		case "list":
 			os.Args = os.Args[2:]
-			parse_option()
-			show_ec2_list(new_session(region, access_key_id, secret_key_id))
+			zaws := NewZaws()
+			zaws.ShowEc2List()
 		default:
 			usage()
 		}
@@ -219,8 +229,8 @@ func main() {
 			switch os.Args[3] {
 			case "ec2":
 				os.Args = os.Args[3:]
-				parse_option()
-				show_cloudwatch_metrics_list(new_session(region, access_key_id, secret_key_id))
+				zaws := NewZaws()
+				zaws.ShowCloudwatchMetricsList()
 			case "rds":
 			case "elb":
 			default:
@@ -228,8 +238,8 @@ func main() {
 			}
 		case "stats":
 			os.Args = os.Args[2:]
-			parse_option()
-			send_metric_stats(new_session(region, access_key_id, secret_key_id))
+			zaws := NewZaws()
+			zaws.SendMetricStats()
 		default:
 			usage()
 		}
